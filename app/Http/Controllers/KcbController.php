4a8f72ad72ac0bbccd7a05262c6ab903e1c8f35d
AlbertWebs <albertmuhatia@gmail.com>
2025-02-19 16:41:00 +0300

    Skush

--- app/Http/Controllers/KcbController.php
+++ app/Http/Controllers/KcbController.php
@@ -81,7 +81,7 @@ class KcbController extends Controller
             $OrderId = $OrderID+1;
         }
 
-        $InvoiceNumber = "ABM-";
+        $InvoiceNumber = "ABM";
         return $InvoiceNumber;
     }
 
@@ -106,7 +106,7 @@ class KcbController extends Controller
         $postData = array(
            "phoneNumber"=> $mobile,
            "amount"=> $amount,
-           "invoiceNumber"=> "5993428#".$invoiceNumber,
+           "invoiceNumber"=> $invoiceNumber,
            "sharedShortCode"=> true,
            "orgShortCode"=> "",
            "orgPassKey"=> "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
